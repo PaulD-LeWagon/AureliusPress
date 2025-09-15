06b7117368c6b4c513a57f369e3be551d59f1424
Paul Devanney <pauldevanney92@gmail.com>
2025-09-15 10:11:29 +0100

    Note Moderation CRUD (RRD) Feature Tests (actually) GREEN this time

--- app/controllers/aurelius_press/admin/fragment/notes_controller.rb
+++ app/controllers/aurelius_press/admin/fragment/notes_controller.rb
@@ -1,6 +1,19 @@
 class AureliusPress::Admin::Fragment::NotesController < AureliusPress::Admin::ApplicationController
   # before_action :authenticate_user!
-  before_action :set_note, only: [:show, :edit, :update, :destroy]
+  before_action :set_note, only: [:show, :destroy] # :edit, :update
+
+  ## Admin interface for managing notes
+  # This section contains actions for managing notes in the admin interface.
+  # Only index, show, and destroy actions are currently enabled as that is all
+  # that is needed for moderation purposes.
+  #
+  # Other actions (new, edit, create, update) are commented out to prevent
+  # creation or modification of notes via the admin interface.
+  # However, when soft deletion is implemented, the update action will be needed.
+  # And a "Reporting" and "User Ban" feature will be required in the future.
+  #
+  # @todo: Implement soft deletion for notes
+  # @todo Add pagination to index action if notes volume is high.
 
   def index
     @notes = AureliusPress::Fragment::Note.all
@@ -10,34 +23,34 @@ class AureliusPress::Admin::Fragment::NotesController < AureliusPress::Admin::Ap
     # @note is set by the before_action :set_note
   end
 
-  def new
-    @note = AureliusPress::Fragment::Note.new
-  end
+  # def new
+  #   @note = AureliusPress::Fragment::Note.new
+  # end
 
-  def create
-    @note = AureliusPress::Fragment::Note.new(note_params)
-    if @note.save
-      redirect_to aurelius_press_admin_fragment_note_path(@note), notice: "Note was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @note = AureliusPress::Fragment::Note.new(note_params)
+  #   if @note.save
+  #     redirect_to aurelius_press_admin_fragment_note_path(@note), notice: action_was_successfully(:created)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def edit
-    # @note is set by the before_action :set_note
-  end
+  # def edit
+  #   # @note is set by the before_action :set_note
+  # end
 
-  def update
-    if @note.update(note_params)
-      redirect_to aurelius_press_admin_fragment_note_path(@note), notice: "Note was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @note.update(note_params)
+  #     redirect_to aurelius_press_admin_fragment_note_path(@note), notice: action_was_successfully(:updated)
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
     @note.destroy
-    redirect_to aurelius_press_admin_fragment_notes_path, notice: "Note was successfully destroyed."
+    redirect_to aurelius_press_admin_fragment_notes_path, notice: action_was_successfully(:deleted)
   end
 
   private
@@ -47,6 +60,10 @@ class AureliusPress::Admin::Fragment::NotesController < AureliusPress::Admin::Ap
     if @note.nil?
       redirect_to aurelius_press_admin_fragment_notes_path, alert: "Note not found."
     end
+  end
+
+  def action_was_successfully(action)
+    "Note #{action} successfully."
   end
 
   def note_params
